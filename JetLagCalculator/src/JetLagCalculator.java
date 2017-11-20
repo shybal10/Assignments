ce2b899d0f5a9bfd1c16cf7b4e2ff1792f782c7d
Shybal <shybalibrahim@gmail.com>
2017-11-20 19:36:00 +0530

    restructured the code and added comments

--- JetLagCalculator/src/JetLagCalculator.java
+++ JetLagCalculator/src/JetLagCalculator.java
@@ -1,47 +1,101 @@
 import java.util.Scanner;
 
+/**
+ * calculates the JetLag suffered by a passenger based on the formulae of the aviation department
+ * @author shybal
+ * @version 1.0
+ */
 public class JetLagCalculator {
+    public static Scanner input = new Scanner(System.in);
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.println("enter the number of hours travelled");
-        double hoursTravelled = input.nextDouble();
-        System.out.println("enter the number of zones crossed");
-        double zonesCrossed = input.nextDouble();
-        System.out.println("enter the arrival time");
-        int arrivalTime = input.nextInt();
-        int arrivalCode = 0;
-        if (arrivalTime < 12 && arrivalTime > 8) {
-            arrivalCode = 4;
-        }
-        else if (arrivalTime < 18 && arrivalTime > 12) {
-            arrivalCode = 2;
-        }
-        else if (arrivalTime < 22 && arrivalTime > 18) {
-            arrivalCode = 0;
-        } else if (( arrivalTime > 22 && arrivalTime < 24 )|| arrivalTime < 1) {
-            arrivalCode = 1;
-        } else if (arrivalTime < 8 && arrivalTime > 1) {
-            arrivalCode = 3;
-        }
-        System.out.println("enter departure time");
-        int departureTime = input.nextInt();
-        int departureCode = 0;
-        if (departureTime < 12 && departureTime > 8) {
-            departureCode = 0;
-        }
-        else if (departureTime < 18 && departureTime > 12) {
-            departureCode = 1;
-        }
-        else if (departureTime < 22 && departureTime > 18) {
-            departureCode = 3;
-        } else if (( departureTime > 22 && departureTime < 24 )|| departureTime < 1) {
-            departureCode = 4;
-        } else if (departureTime < 8 && departureTime > 1) {
-            departureCode = 5;
-        }
-        System.out.println(getJetLag(hoursTravelled,zonesCrossed,departureCode,arrivalCode));
+        int arrivalTime = getArrivalTime();
+        int departureTime = getDepartureTme();
+        System.out.println(getJetLag(getHoursTravelled(),getZonesCrossed(),generateDepartureCode(departureTime),generateArrivalCode(arrivalTime)));
     }
+
+    /**
+     * finds the number of days of jet lag suffered
+     * @param hours is the total number of hours travelled by the passenger
+     * @param zonesCrossed is the number of zones crossed by the passenger
+     * @param departureCode is the code generated based on departure time
+     * @param arrivalCode is the code generated based on the arrival time
+     * @return the number of days in double format
+     */
     private static double getJetLag(double hours, double zonesCrossed, int departureCode, int arrivalCode) {
         return  (hours/2 + (zonesCrossed-3) + departureCode + arrivalCode)/10;
+    }
+
+    /**
+     * calculates and generates an arrival code
+     * @param arrivalTime of the passenger as int
+     * @return the generated arrival code
+     */
+    private static int generateArrivalCode(int arrivalTime) {
+        if (arrivalTime < 12 && arrivalTime >= 8) {
+            return  4;
+        }
+        else if (arrivalTime < 18 && arrivalTime >= 12) {
+            return  2;
+        }
+        else if (arrivalTime < 22 && arrivalTime >= 18) {
+            return  0;
+        } else if (( arrivalTime >= 22 && arrivalTime <= 24 )|| arrivalTime < 1) {
+            return  1;
+        } else  {
+            return  3;
+        }
+    }
+
+    /**
+     * calculates and generates a departure code
+     * @param departureTime of the passenger as int
+     * @return the generated departure code
+     */
+    private static int generateDepartureCode(int departureTime) {
+        if (departureTime < 12 && departureTime >= 8) {
+            return  0;
+        }
+        else if (departureTime < 18 && departureTime >= 12) {
+            return  1;
+        }
+        else if (departureTime < 22 && departureTime >= 18) {
+            return  3;
+        } else if (( departureTime >= 22 && departureTime <= 24 )|| departureTime < 1) {
+            return 4;
+        } else  {
+            return 5;
+        }
+    }
+
+    /**
+     * @return the number of hours traveled by passenger as double
+     */
+    private static double getHoursTravelled() {
+        System.out.println("enter the number of hours travelled");
+        return input.nextDouble();
+    }
+
+    /**
+     * @return the number of zones crossed by the passenger as double
+     */
+    private static double getZonesCrossed() {
+        System.out.println("enter the number of zones crossed");
+        return  input.nextDouble();
+    }
+
+    /**
+     * @return the arrival time of the passenger as int
+     */
+    private static int getArrivalTime() {
+        System.out.println("enter the arrival time");
+        return input.nextInt();
+    }
+
+    /**
+     * @return the departure time of the passenger as int
+     */
+    private static int getDepartureTme() {
+        System.out.println("enter departure time");
+        return input.nextInt();
     }
 }
