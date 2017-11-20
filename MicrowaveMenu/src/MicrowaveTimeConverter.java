0e148f40077f16a9c6495887bcbca0bdc7cffbcb
Shybal <shybalibrahim@gmail.com>
2017-11-20 19:39:06 +0530

    added comments

--- MicrowaveMenu/src/MicrowaveTimeConverter.java
+++ MicrowaveMenu/src/MicrowaveTimeConverter.java
@@ -1,5 +1,8 @@
 import java.util.Scanner;
 
+/**
+ * converts the microwave timer set into a readable format
+ */
 public class MicrowaveTimeConverter {
     public static void main(String[] args) {
         System.out.print("enter cook time --> ");
@@ -7,6 +10,12 @@ public class MicrowaveTimeConverter {
         int time = input.nextInt();
         System.out.println("your time --> "+getMicrowaveTime(time));
     }
+
+    /**
+     * function that performs the formatting
+     * @param time is the non- formatted time as int
+     * @return formatted time as string
+     */
     private static String getMicrowaveTime(int time) {
         int minutesPlace = time / 100;
         int secondsPlace = time % 100;
