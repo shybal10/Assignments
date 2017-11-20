c6012f089e63eb8aab0ba3c075dd98de3c88fb87
Shybal <shybalibrahim@gmail.com>
2017-11-20 19:49:02 +0530

    added comments with efficient code

--- PanagramValidator/src/PanagramValidator.java
+++ PanagramValidator/src/PanagramValidator.java
@@ -1,5 +1,11 @@
 import java.util.Scanner;
 
+/**
+ * class finds whether the sentence is a Panagram
+ * a sentence is said to be a panagram if it includes all the letters of the alphabet
+ * @author shybal
+ * @version 1.0
+ */
 public class PanagramValidator {
     public static void main(String[] args) {
         System.out.println("enter the sentence to be checked");
@@ -11,6 +17,12 @@ public class PanagramValidator {
             System.out.println("not a Pangram");
         }
     }
+
+    /**
+     * function checks whether the sentence is a panagram
+     * @param sentence is the input string
+     * @return true if the sentence is a panagram
+     */
     private static boolean isPangram(String sentence) {
         for (char character = 'a'; character <= 'z'; character++) {
             if (sentence.indexOf(character) < 0)
