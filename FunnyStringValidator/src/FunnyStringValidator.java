1b3096f03279d27e0691435114ee82a342eded2a
Shybal <shybalibrahim@gmail.com>
2017-11-20 18:35:58 +0530

    added comments

--- FunnyStringValidator/src/FunnyStringValidator.java
+++ FunnyStringValidator/src/FunnyStringValidator.java
@@ -1,4 +1,9 @@
 import java.util.Scanner;
+/**
+ * validates whether a string qualifies as funny or not
+ * @author shybal
+ * @version 1.0
+ */
 public class FunnyStringValidator {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
@@ -15,6 +20,13 @@ public class FunnyStringValidator {
             }
         }
     }
+
+    /**
+     * the funniness of the string is validated here
+     * @param strings original string taken as input
+     * @param reverse is the reverse of the original string
+     * @return returns true if the string is funny
+     */
     private static boolean isFunny(String strings,String reverse) {
             for (int j = 1; j < strings.length(); j++) {
                 if (Math.abs(strings.charAt(j) - strings.charAt(j-1)) != Math.abs(reverse.charAt(j) - reverse.charAt(j-1))) {
@@ -23,6 +35,12 @@ public class FunnyStringValidator {
             }
         return true;
     }
+
+    /**
+     * finds the reverse of the input string
+     * @param string original string taken as input
+     * @return the reverse of the original string
+     */
     private static String getReverse(String string) {
         String reverse = "";
         for (int i = string.length() - 1; i >= 0; i--) {
