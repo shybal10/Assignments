108435fa8d466bdacd0601f4fe36eb739235c965
Shybal <shybalibrahim@gmail.com>
2018-02-26 16:44:04 +0530

    added random monthly payments

--- MonthlyPayment/src/MonthlyPaymentCalculator.java
+++ MonthlyPayment/src/MonthlyPaymentCalculator.java
@@ -1,3 +1,4 @@
+
 /**
  * gets the payment status after each month
  * @author shybal
@@ -5,24 +6,27 @@
  */
 public class MonthlyPaymentCalculator {
     public static void main(String[] args) {
-        CalculatePayment(100);
+        // Math.random() * (MAX - MIN) + MIN
+        CalculatePayment( Math.random() * 99 + 1);
     }
 
     /**
      * calculate payment status after each month
      * @param monthlyPayment paid each month
      */
-    private static void CalculatePayment(int monthlyPayment) {
+    private static void CalculatePayment(double monthlyPayment) {
         double balance = 1000;
-        int i = 1;
+        int month = 1;
+        System.out.println(monthlyPayment);
         double totalPayments = 0;
         while (balance >= 0) {
-            System.out.print("month: "+i+"\t");
+            System.out.print("month: "+month+"\t");
             balance = balance + (balance * (1.5/100)) - monthlyPayment;
             totalPayments = totalPayments + monthlyPayment;
-            System.out.print("balance: "+balance);
-            System.out.println("\t\t\t\t total payments: "+totalPayments);
-            i++;
+            System.out.printf("balance: %.2f",balance);
+            System.out.printf("\t\t\t\t total payments: %.2f",totalPayments);
+            System.out.println();
+            month++;
         }
     }
 }
